b641c49f70fd235134e7cee66eedd66c4c773b79
limeblue517 <coke3795@naver.com>
2025-07-11 19:41:07 +0900

    0711

---
+++ rdgsolo/Assets/Script/Look Monster.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+using UnityEngine.Animations;
+
+public class LookMonster : MonoBehaviour
+{
+    private GameObject monster;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        monster = GameObject.FindWithTag("monster");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        /*transform.LookAt();*/
+    }
+}
